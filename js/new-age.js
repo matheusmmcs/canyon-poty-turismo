dad067ff8a990c7b466cc896e0830cbc103602a6
Matheus <matheuscampanha@ufpi.edu.br>
2017-10-21 18:44:03 -0300

    Initial commit

---
+++ js/new-age.js
@@ -0,0 +1,47 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+    // Closes responsive menu when a link is clicked
+    $('.navbar-collapse>ul>li>a').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    $('.navbar-toggler').on('click', function(e) {
+        var hasCollapsed = !($(this).attr('aria-expanded') == 'true');
+        if (hasCollapsed) {
+            $(this).parent().addClass('canyon-navbar-collapsed');
+        } else {
+            $(this).parent().removeClass('canyon-navbar-collapsed');
+        }
+    });
+
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    });
+
+})(jQuery); // End of use strict
